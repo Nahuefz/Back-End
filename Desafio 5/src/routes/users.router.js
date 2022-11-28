6158449f6ed542b69e7211120601d27cd21fbb7f
Nahuel <nahuelfontenez@gmail.com>
2022-11-28 14:47:54 -0300

    Clase 9 motores de plantillas

---
+++ Desafio 5/src/routes/users.router.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import UsersManager from "../managers/users.manager.js";
+
+const router = Router();
+const usersService = new UsersManager();
+
+router.post("/", async (req, res) => {
+  const { name, email, age } = req.body;
+  if (!name || !email || !age)
+    return res
+      .status(400)
+      .send({ status: "error", error: "Incomplete values" });
+  const user = {
+    name,
+    email,
+    age,
+  };
+  let result = await usersService.saveUser(user);
+  res.send({ status: "success", payload: result });
+});
+
+export default router;
